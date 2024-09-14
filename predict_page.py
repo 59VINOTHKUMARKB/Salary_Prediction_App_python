85125cad638232ad7ac4fc3b8f1d356ef2b81f4e
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2024-09-14 15:42:19 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -14,8 +14,7 @@ le_country = data["le_country"]
 le_education = data["le_education"]
 
 def show_predict_page():
-    st.title("Software Developer Salary Prediction")
-    st.subtitle("Based on StackOverflow Survey data")
+    st.title("Software Developer Salary Predictions")
     st.write("""\t\tWe need some information to predict the salary""")
     countries = (
         "United States of America",
